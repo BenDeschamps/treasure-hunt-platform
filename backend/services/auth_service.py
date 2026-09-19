1519b6041b74f1890fb696d8ad9daaf5b0a2ffec
BenDeschamps <benjamin.deschamps1309@gmail.com>
2026-09-19 13:12:33 +0200

    Fix JWT algorithm

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -5,7 +5,7 @@ from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+ALGORITHM = "HS256"
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
